26392208c649b53d7bda08a832a039f7410081bc
李靈 <lin_li@cmoney.com.tw>
2025-04-27 20:45:53 +0800

    chore:更新版本號至1.0.2
    將versionCode升級至5，versionName更新為1.0.2。此變更為準備釋出新版本，以提供最新功能或修正。`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.linli.happyplace"
         minSdk = 23
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
