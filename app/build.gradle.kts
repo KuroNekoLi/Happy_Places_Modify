0d62188ce9d3d4e1224f45190ca07539402cc99c
李靈 <lin_li@cmoney.com.tw>
2025-05-01 18:28:59 +0800

    feat: 更新版本號至1.1.0
    將versionCode提高至7，versionName更新為1.1.0。此變更準備發布新版本，包含近期的功能與修正內容。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.linli.happyplace"
         minSdk = 23
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.3"
+        versionCode = 7
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
