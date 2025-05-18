171767dd8475c2ed0012edfb535cfe37e23c456e
李靈 <lin_li@cmoney.com.tw>
2025-05-18 11:42:31 +0800

    feat: 更新版本號至1.2.0。
    調整了versionCode與versionName以反映新版應用程式的更新。此更改有助於區分不同版本，並確保應用程式打包與發布時使用正確的版本資訊。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.linli.happyplace"
         minSdk = 23
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.1.0"
+        versionCode = 8
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
