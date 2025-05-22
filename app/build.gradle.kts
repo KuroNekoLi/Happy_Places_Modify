d746246d5769a3bfd2d499ce73a8d34c96555325
李靈 <lin_li@cmoney.com.tw>
2025-05-22 11:33:21 +0800

    chore: 更新版本號至1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.linli.happyplace"
         minSdk = 23
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.2.2"
+        versionCode = 11
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
